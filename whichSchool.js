f39c0847a0b96e2e1fb35ddfe2160f07bacac102
Diogenesoftoronto <diogenesoft@protonmail.com>
2021-11-14 20:14:42 +0000

    hello function for lighthouse prep

--- whichSchool.js
+++ whichSchool.js
@@ -1,13 +1,13 @@
-const whichSchool = function(age){
-    if(age < 13) {
-        return "Elementary School";
-    }
-    else if (age >= 13 && age < 18) {
-        return "Secondary School";
-    }
-    else {
-        return "Lighthouse Labs";
-    }
+function whichSchool(age){
+  if(age < 13) {
+    return "Elementary School";
+  }
+  else if (age >= 13 && age < 18) {
+    return "Secondary School";
+  }
+  else {
+    return "Lighthouse Labs";
+  }
 }
 console.log("I am 35. Which school should I go to?");
 console.log(whichSchool(35));
